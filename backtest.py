fd2577edb0a27dc3134f9052166059d964623406
jonghwanl-byte <jonghwanl@gmail.com>
2026-04-30 08:15:06 +0900

    Adjust threshold for p_vals comparison in backtest

--- backtest.py
+++ backtest.py
@@ -53,9 +53,9 @@ def run_backtest():
             for i in range(len(p_vals)):
                 if np.isnan(m_vals[i]):
                     continue
-                if p_vals[i] > m_vals[i] * 1.02:
+                if p_vals[i] > m_vals[i] * 1.04:
                     curr_state = 1
-                elif p_vals[i] < m_vals[i] * 0.98:
+                elif p_vals[i] < m_vals[i] * 0.96:
                     curr_state = 0
                 state[i] = curr_state
                 
